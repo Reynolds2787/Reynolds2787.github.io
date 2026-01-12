c64e8ba40c8cdc50451c65eed671b191ff879081
Reynolds2787 <sam.beutelspacher@gmail.com>
2026-01-12 10:30:05 +0000

    bugfix

--- js/logout.js
+++ js/logout.js
@@ -16,7 +16,15 @@ document.addEventListener("DOMContentLoaded", () => {
     localStorage.removeItem("refresh_token");
     localStorage.removeItem("username");
 
-    window.location.href = "https://efmapp.co.uk/"
+    const COGNITO_DOMAIN =
+      "https://eu-west-26tjf0zbnc.auth.eu-west-2.amazoncognito.com";
+    const CLIENT_ID = "63ag0fj4bpqi4ouvat8bsc0890";
+
+    const logoutUrl =
+      `${COGNITO_DOMAIN}/logout` +
+      `?client_id=${CLIENT_ID}` +
+      `&logout_uri=${encodeURIComponent(window.location.origin)}`;
+
+    window.location.href = logoutUrl;
   });
-  
 });
