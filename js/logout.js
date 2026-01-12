53819499e387bb03e25ea5e955f46a310cf48146
Reynolds2787 <sam.beutelspacher@gmail.com>
2026-01-12 10:22:09 +0000

    bugfix

--- js/logout.js
+++ js/logout.js
@@ -16,9 +16,7 @@ document.addEventListener("DOMContentLoaded", () => {
     localStorage.removeItem("refresh_token");
     localStorage.removeItem("username");
 
-    window.location.href =
-      "https://cognito-idp.eu-west-2.amazonaws.com/eu-west-2_6tjf0zBnC" +
-      "?client_id=63ag0fj4bpqi4ouvat8bsc0890" +
-      "&logout_uri=" + encodeURIComponent("https://efmapp.co.uk/");
+    window.location.href = "https://efmapp.co.uk/"
   });
+  
 });
